e7b1b46f38409805da67af09ad05df2ff39d3161
nandha <nandhakumar.learnings@gmail.com>
2019-04-29 21:55:24 -0400

    Sample lamda

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        
+      final  List<BigDecimal> prices = new ArrayList<BigDecimal>();
+        prices.add(BigDecimal.valueOf(10));
+        prices.add(BigDecimal.valueOf(20));
+        prices.add(BigDecimal.valueOf(30));
+        prices.add(BigDecimal.valueOf(40));
+        prices.add(BigDecimal.valueOf(50));
+        prices.add(BigDecimal.valueOf(60));
+     final   BigDecimal finalPrice = prices.stream().filter(price -> price.compareTo(BigDecimal.valueOf(50)) > 0)
+                            .map(price -> {
+                                return (price.multiply(BigDecimal.valueOf(10)));
+                            })
+                            .reduce(BigDecimal.ZERO, BigDecimal::add );
+
+     System.out.println(finalPrice);
+
+        prices.forEach(System.out::println);
+
     }
 }
